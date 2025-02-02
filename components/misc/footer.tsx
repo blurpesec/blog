251dfc877f017e61f996719853bde7f7e134c709
Michael Hahn <hahn.michael.f@gmail.com>
2025-02-01 16:02:24 -0800

    chore: remove footer

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+        {/* <div className="py-28 flex flex-col lg:flex-row items-center">
+          {/* <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             <PreviewLink href="/statically-generated">Statically Generated</PreviewLink> with Next.js.
-          </h3>
+          </h3> 
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="#"
@@ -21,11 +21,10 @@ const Footer = () => {
             >
               View on GitHub
             </a>
-          </div>
-        </div>
+          </div>*/}
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
